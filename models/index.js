eed403a0fab328405516213bf446baaadf67e496
jiUmin <wjdwkjh94@gmail.com>
2023-05-02 01:15:55 +0900

    hide_testfile

--- models/index.js
+++ models/index.js
@@ -19,7 +19,10 @@ fs.readdirSync(__dirname) // 현재 폴더의 모든 파일을 조회
   .filter((file) => {
     // 숨김 파일, index.js, js 확장자가 아닌 파일 필터링
     return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+      file.indexOf(".") !== 0 &&
+      !file.includes("test") &&
+      file !== basename &&
+      file.slice(-3) === ".js"
     );
   })
   .forEach((file) => {
